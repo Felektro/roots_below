a63689169c6566d34f2fb97bb8d59ab9167dee93
skelet Przemek <julia.best.vv@gmail.com>
2026-04-01 14:34:35 -0400

    Added multiple screens
    Finish splash screen. Most of the code is missing

---
+++ Roots_Below/Menu.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Menu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Menu extends World
+{
+
+    /**
+     * Constructor for objects of class Menu.
+     * 
+     */
+    public Menu()
+    {    
+        // Create a new world same size as the gaming screen
+        super(1600, 900, 1);
+        //prepare();
+    }
+} 
+ 
---
+++ Roots_Below/SplashScreen.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SplashScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SplashScreen extends World
+{
+    
+    
+    public SplashScreen()
+    {    
+        super(1600, 900, 1);
+        GreenfootImage image = new GreenfootImage("vanier_Logo.png");
+        getBackground().drawImage(image, (1600 - image.getWidth())/2, (900 - image.getHeight())/2);
+        showText("Game Programming 1 - 420-141-VA - Winter 2026", 100 , 100);
+        showText("Illia, Yuliia, Richard", 800 ,100);
+        
+    }
+
+    public void showText(String message, int x, int y)
+    {
+        GreenfootImage bg = getBackground();
+        Font font =  new  Font(50);
+        bg.setFont(font);
+        bg.setColor(Color.WHITE);
+        bg.drawString(message, x, y);
+    }
+
+}
